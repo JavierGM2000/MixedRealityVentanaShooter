5dc2afd242fa385bbb01caf153bd88bea95f906e
Javier Gomez <gomez.javier@opendeusto.com>
2024-05-12 21:26:12 +0200

    Cambios fire ray

---
+++ Assets/@MyAssets/Scripts/FireRayCast.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FireRayCast : MonoBehaviour
+{
+    int counter = 0;
+    [SerializeField]
+    TMP_Text marcador;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    public void fireGun()
+    {
+        RaycastHit hit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
