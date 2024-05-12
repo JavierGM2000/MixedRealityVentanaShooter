99ae62dae534259a5470fb1cf1331461f23ea879
Javier Gomez <gomez.javier@opendeusto.com>
2024-05-12 21:50:43 +0200

    Función poner ventana

--- Assets/@MyAssets/Scripts/FireRayCast.cs
+++ Assets/@MyAssets/Scripts/FireRayCast.cs
@@ -8,12 +8,34 @@ public class FireRayCast : MonoBehaviour
     int counter = 0;
     [SerializeField]
     TMP_Text marcador;
-    
+
+    [SerializeField]
+    GameObject WindowPrefab;
+    GameObject instanciaWindow = null;
+
+    Transform rayOrig;
 
     // Start is called before the first frame update
     void Start()
     {
             
+    }
+
+    public void placeWindow()
+    {
+        if (instanciaWindow)
+        {
+            return;
+        }
+
+        int layerMask = LayerMask.NameToLayer("Wall");
+        RaycastHit hit;
+        if (Physics.Raycast(rayOrig.position, rayOrig.forward, out hit, 10f, layerMask))
+        {
+            Debug.Log("Did Hit");
+            instanciaWindow = Instantiate(WindowPrefab, hit.point,Quaternion.identity);
+            instanciaWindow.transform.rotation = Quaternion.FromToRotation(Vector3.forward, hit.normal);
+        }
     }
 
     public void fireGun()
